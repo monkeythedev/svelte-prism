471ecc4df86ab605ddbe3abf3005974a18a8817b
Ray Thurne Void <ray.thurne.void@gmail.com>
2021-06-27 03:37:44 +0200

    chore: typescript conversion + build upgrade + dev command to build & watch w/ nodemon + deps update + webpack example

---
+++ src/lib/Prism.svelte
@@ -0,0 +1,66 @@
+<script context="module" lang="ts">
+	import _prism from "./import";
+	export const prism = _prism;
+	export const highlight = _prism.highlightElement;
+	export const globalConfig = { transform: (x) => x };
+</script>
+
+<script lang="ts">
+	import "prism-svelte";
+	import { onMount, onDestroy } from "svelte";
+
+	export let language: string = "javascript";
+	export let source: string = "";
+	export let transform: (src: string) => string = (src) => src;
+
+	let element: HTMLDivElement;
+	let formattedCode: string;
+
+	let elementObserver: MutationObserver;
+	onMount(() => {
+		elementObserver = new MutationObserver((mutations) => {
+			const addedChilds = mutations.find(
+				(mutation) =>
+					mutation.type === "childList" && mutation.addedNodes.length
+			);
+			const charData = mutations.find(
+				(mutation) => mutation.type === "characterData" && mutation.target
+			);
+			if (addedChilds) {
+				source = addedChilds.target.textContent;
+			} else {
+				source = charData.target.textContent;
+			}
+		});
+
+		elementObserver.observe(element, {
+			childList: true,
+			characterData: true,
+			subtree: true,
+		});
+	});
+
+	onDestroy(() => {
+		elementObserver?.disconnect();
+	});
+
+	$: source != null && highlightCode(source);
+
+	function highlightCode(source: string) {
+		const grammar = prism.languages[language];
+		let body = source;
+		body = globalConfig.transform(body);
+		body = transform(body);
+		formattedCode =
+			language === "none" ? body : prism.highlight(body, grammar, language);
+	}
+</script>
+
+<code bind:this={element} style="display:none">
+	<slot />
+</code>
+
+<pre class="language-{language}" command-line data-output="2-17"><code
+		class="language-{language}"
+		>{#if language === "none"}{formattedCode}{:else}{@html formattedCode}{/if}</code
+	></pre>
